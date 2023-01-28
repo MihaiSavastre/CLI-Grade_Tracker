1e317a3e20a444acd7c52cea68f03294d285c99c
Mihai Savastre <mihai.savastre99@gmail.com>
2023-01-28 06:44:10 +0200

    First commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        commandLoop();
+    }
+
+    private static void commandLoop() {
+        System.out.println("Learning Progress Tracker");
+        String input;
+        while (scanner.hasNextLine()) {
+            input = readLine();
+            Commands.executeCommand(input);
+        }
+    }
+
+    public static String readLine() {
+        return scanner.nextLine().strip().toLowerCase();
+    }
+}
+
